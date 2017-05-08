eb70225fbac93050456fae6b8067e2bae171e606
KaHin Ng and Nicholas Barron <pair+kang+nbarron@pivotal.io>
2017-05-08 10:36:19 -0400

    initial set up

---
+++ swift-earlgrey-bootcamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-earlgrey-bootcamp
+//
+//  Created by Pivotal on 2017-05-08.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
