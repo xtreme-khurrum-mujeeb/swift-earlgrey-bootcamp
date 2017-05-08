2472cfbdbb0ed47a624691b47c43eae2584b6e6f
KaHin Ng and Nicholas Barron <pair+kang+nbarron@pivotal.io>
2017-05-08 11:08:38 -0400

    add Step 1 screen default field behaviour

--- swift-earlgrey-bootcamp/ViewController.swift
+++ swift-earlgrey-bootcamp/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var step1Label: UILabel!
+    @IBOutlet weak var step1TextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // tie in text field to delegate
+        step1TextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+    
+    // MARK: Actions
+    @IBAction func step1Button(_ sender: UIButton) {
+        step1Label.text = "Button pressed"
+    }
+    
 }
 
